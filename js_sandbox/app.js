5a52ce3158be630ec94361be737a972df4c00bc7
GproSoftware828 <JetSendProductions@Gmail.com>
2018-12-21 16:38:55 +1100

    Streamlined app for submitting and viewing JSON posts

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,48 +1,33 @@
-document.getElementById('button1').addEventListener('click', getText);
-document.getElementById('button2').addEventListener('click', getJson);
-document.getElementById('button3').addEventListener('click', getExternal);
-
-// Get local text file data
-function getText() {
-  fetch('test.txt')
-    .then(res => res.text());
-    .then(data => {
-      console.log(data);
-      document.getElementById('output').innerHTML = data;
-    })
-    .catch(err => console.log(err);
-    });
-}
+document.getElementById('button1').addEventListener('Submit', getJson);
 
 // Get local json data
 function getJson() {
   fetch('posts.json')
-    .then (res => res.json())
+    .then(res => res.json())
     .then(data => {
       console.log(data);
       let output = '';
-      data.forEach(function(post) {
-        output += `<li>${post.title}</li>`;
-      })
+      data.forEach(post => {
+        output += `<li>${post.title}</li><br /><li>${post.body}</li>`;
+      });
       document.getElementById('output').innerHTML = data;
     })
-    // .then (data => forEach.post
-    //   (output += `<li>${post.title}</li>`)
-    //   let output = '')
     .catch(err => console.log(err));
-}
+};
 
-// Get from external API
-function getExternal() {
-  fetch('https://api.github.com/users')
-    .then(res => res.json());
-    .then(data => {
-      console.log(data);
-      let output = '';
-      data.forEach(function(user) {
-        output += `<li>${user.login}</li>`;
-      });
-      document.getElementById('output').innerHTML = output;
+document.addEventListener('button2').addEventListener('Submit', postData);
+
+postData('posts.json', data) {
+  return new Promise((resolve, reject) => {
+    fetch('./posts.json', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
     })
-    .catch(err => console.log(err));
-}
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
+};
