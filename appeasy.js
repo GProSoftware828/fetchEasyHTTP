20fcee349cc0c4710fe8dd57f6aefaccfab2fc12
GproSoftware828 <JetSendProductions@Gmail.com>
2019-01-01 15:20:21 +1100

    Minor refacotring

--- appeasy.js
+++ appeasy.js
@@ -1,23 +1,23 @@
-const http = new EasyHTTP;
-
-http.get('#')
-	.then(data => console.log(data))
-	.catch(err => console.log(err));
-
 const data = {
 	name: 'John Doe',
 	username: 'johndoe',
 	email: 'jdoe@gmail.com'
 }
 
-http.post('#', data)
+const http = new EasyHTTP;
+
+http.get('/js_sandbox/posts.json')
 	.then(data => console.log(data))
 	.catch(err => console.log(err));
 
-http.put('#/#', data)
+http.post('/js_sandbox/posts.json', data)
 	.then(data => console.log(data))
 	.catch(err => console.log(err));
 
-http.delete('#/#')
+http.put('/js_sandbox/posts.json/1', data)
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
+
+http.delete('/js_sandbox/posts.json/1')
 	.then(data => console.log(data))
 	.catch(err => console.log(err));
