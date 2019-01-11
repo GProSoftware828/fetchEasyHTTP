261c9880047c2975aee0f196038fceb7bc0bd2fc
GproSoftware828 <JetSendProductions@Gmail.com>
2019-01-11 21:08:14 +1100

    Experiments with routes and server paths

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,16 +1,15 @@
-document.getElementById('button1').addEventListener('Submit', getJson);
-function getJson() {
-  fetch('posts.json')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      let output = '';
-      data.forEach(post => {
-        output += `<li>${post.title}</li><br /><li>${post.body}</li>`;
-      });
-      document.getElementById('output').innerHTML = data;
-    })
-    .catch(err => console.log(err));
+document.getElementById('button1').addEventListener('Submit', postData);
+
+async postData('posts.json', data) {
+  const response = await fetch(url,
+    {
+      method: 'POST', headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  const resData = await response.json();
+  return resData;
 };
 
 document.addEventListener('button2').addEventListener('Submit', postData);
@@ -27,4 +26,20 @@ postData('posts.json', data) {
       .then(data => resolve(data))
       .catch(err => reject(err));
   })
+};
+
+document.getElementById('button3').addEventListener('Submit', getText);
+
+function getText() {
+  fetch('test.txt')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let output = '';
+      data.forEach(post => {
+        output += `<li>${post.title}</li><br /><li>${post.body}</li>`;
+      });
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(err => console.log(err));
 };
